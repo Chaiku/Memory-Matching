bce15e8cdf4f4b8c8eb5d61c1f2fa4a008b9e9f0
Barry Scott Rollan <barry.rollan@gmail.com>
2019-08-05 15:48:59 -0400

    added timer in place of header once game play starts

--- memory-matching/src/Game.js
+++ memory-matching/src/Game.js
@@ -4,14 +4,60 @@ import './game.css';
 import Board from './Board';
 
 class Game extends React.Component {
+  state = {
+    time: 90,
+    gameStarted: false,
+  }
+
+  youLose = () => {
+    document.getElementById('youLose').display = 'inline-block'
+  }
+
+  subtractSecond = () => {
+    this.setState({ time: this.state.time - 1 });
+    this.startTimer();
+  }
+
+  startTimer = () => {
+    setTimeout(() => {
+      if(this.state.time > 0 ) {
+      this.subtractSecond();
+      }else{
+      this.youLose();
+      }
+  }, 1000);
+  }
+
+  openTimer = () => {
+    document.getElementById('gameHead').style.display = "none";
+    document.getElementById('timer').style.display = "inline-block";
+    this.startTimer();
+  };
+
+  handleClick = () => {
+    if(this.state.gameStarted === false){
+      this.setState({ gameStarted: true });
+      this.openTimer();
+    };
+  };
+
+
   render() {
     return (
-      <div className="game">
-        <header className="gameHead">
+      <div 
+        className="game"
+        onClick={this.handleClick}
+        >
+        <header 
+          id="timer"
+          style={{display: 'none'}}
+          >{this.state.time}</header>
+        <header id="youLose" style={{ display: 'none' }}>Time is out, sucka.</header>
+        <header id="gameHead">
           <h1><u>Memory Match</u></h1>
           <p>Click the cards to find their matching counterpart!</p>
         </header>
-
+        
         <Board />
 
       </div>
